888f944adbc59bafe0187d453745d384f1471124
Yananart <yananart@yananart.cn>
2021-07-27 00:24:46 +0800

    添加模版参数控制逻辑

--- src/main/java/cn/yananart/site/api/HomeApi.java
+++ src/main/java/cn/yananart/site/api/HomeApi.java
@@ -5,6 +5,8 @@ import cn.yananart.framework.annotation.HttpApi;
 import cn.yananart.framework.commons.ResponseType;
 import io.vertx.ext.web.RoutingContext;
 
+import java.time.LocalDate;
+
 /**
  * 主页
  *
@@ -16,12 +18,25 @@ public class HomeApi {
 
     @ApiMapping(type = ResponseType.TEMPLATE)
     public void index(RoutingContext context) {
+        var localDate = LocalDate.now();
+        var year = localDate.getYear();
+        context.put("year", year);
         context.next();
     }
 
 
     @ApiMapping(path = "/photo", type = ResponseType.TEMPLATE)
     public void photos(RoutingContext context) {
+        var localDate = LocalDate.now();
+        var year = localDate.getYear();
+        context.put("year", year);
+        context.next();
+    }
+
+
+    @ApiMapping(path = "/admin", type = ResponseType.TEMPLATE)
+    public void admin(RoutingContext context) {
+        // todo 授权
         context.next();
     }
 
